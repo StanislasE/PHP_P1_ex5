f639911c92ba2a209cee7e85d7d1732b3bf6e33b
StanislasE <stanislas.etienne@gmail.com>
2020-11-18 15:14:47 +0100

    fait

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 5</title>
+</head>
+<body>
+    
+<?php 
+    $answer = 'yes';
+
+    if ($answer == "yes"){  // == sert pour une comparaison
+        echo 'Vous avez répondu "oui"';
+    }
+    else {
+        echo 'Vous avez répondu "non"';
+    }
+?>
+
+</body>
+</html>
